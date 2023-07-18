db32763da50c92c41da2bd95770c78c009a31cd9
Federico Gomez <gomez00federico@gmail.com>
2023-07-18 01:20:14 -0300

    mounted routers

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 # FastAPI
 from fastapi import FastAPI
 
+# routers
+from routers import token, users
+
 
 app = FastAPI()
+app.include_router(token.router)
+app.include_router(users.router)
 
 @app.get("/")
 async def root():
